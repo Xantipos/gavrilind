1f2ef3e90bd2d0f57b22314bb9d65b2243b84251
Xantipos <anubite@bk.ru>
2017-11-27 17:45:29 +0300

    1.3 Java doc #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,9 +1,22 @@
 package ru.job4j;
+/**
+* Класс выводит строку в консоль
+* @author gavrilind
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
 
 public class Calculate {
+
 
 public static void main(String[] args)
 {
 System.out.println("Hello world!!!!!!!!!!");
 }
                               } 
+
+/**
+* выводим строку на экран
+* нет аргументов
+*/
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriy Gavrilin (anubite@bk.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
