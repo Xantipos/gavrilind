8908e90e21ac57b7b2a0ba07cd48e949a7303a6b
Xantipos <anubite@bk.ru>
2017-11-29 10:23:04 +0300

    1.4 Junit #4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+//package ru.job4j;
 /**
 * Класс выводит строку в консоль
 * @author gavrilind
@@ -14,9 +14,20 @@ public static void main(String[] args)
 {
 System.out.println("Hello world!!!!!!!!!!");
 }
-                              } 
+                              
 
 /**
 * выводим строку на экран
 * нет аргументов
 */
+/**
+* Method echo.
+* @param name Your name.
+* @return Echo plus your name.
+*/
+public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+}
+
+
+                             } 
