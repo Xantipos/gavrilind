c7be4124d35cde2dc4f56351e3ac3f7d34dc2238
Xantipos <anubite@bk.ru>
2017-11-29 13:41:40 +0300

    1.5 Chekstyle #4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -10,8 +10,7 @@ package ru.job4j;
 public class Calculate {
 
 
-public static void main(String[] args)
-{
+public static void main(String[] args) {
 System.out.println("Hello world!!!!!!!!!!");
 }
                               
