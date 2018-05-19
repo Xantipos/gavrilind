a37846a6e9df04166fb5049c38d20bb3302168d3
Xantipos <anubite@bk.ru>
2018-05-19 16:24:19 +0300

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/array/TwoSort.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoSort.java
@@ -9,61 +9,21 @@ public class TwoSort {
 
     public static int[] sort(int[] array1, int[] array2) {
 
-        int[] result = new int[array1.length + array2.length];
-        int len3 = array1.length+array2.length-1;
-        int len1=array1.length - 1;
-        int len2=array2.length - 1;
 
-        if(len1 > len2){
-            int dev = len1 - len2;
-            while(dev > 0) {
-                result[len3] = array1[len1];
-                dev--;
-                len3--;
-                len1--;
-            }
-
-            int index3 = 0;
-            for (int index = 0; index < (array2.length); index++) {
-                if (array1[index] > array2[index]) {
-                    result[index3] = array2[index];
-                    index3++;
-                    result[index3] = array1[index];
-                    index3++;
-                }
-            }
-        }
-        else if(len2>len1){
-            int dev2 = len2 - len1;
-            while(dev2 > 0) {
-                result[len3] = array2[len2];
-                dev2--;
-                len3--;
-                len2--;
-            }
-
-            int index3 = 0;
-            for (int index = 0; index < (array1.length); index++) {
-                if (array1[index] > array2[index]) {
-                    result[index3] = array2[index];
-                    index3++;
-                    result[index3] = array1[index];
-                    index3++;
-                }
-            }
-        }
-
-       else if(len1 == len2){
-            int index3 = 0;
-            for (int index = 0; index < (array1.length); index++) {
-                if (array1[index] > array2[index]) {
-                    result[index3] = array2[index];
-                    index3++;
-                    result[index3] = array1[index];
-                    index3++;
-                }
+        int len_1 = array1.length, len_2 = array2.length;
+        int a = 0, b = 0;
+        int len = len_1 + len_2;
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (b < len_2 && a < len_1) {
+                if (array1[a] > array2[b]) result[i] = array2[b++];
+                else result[i] = array1[a++];
+            } else if (b < len_2) {
+                result[i] = array2[b++];
+            } else {
+                result[i] = array1[a++];
             }
         }
         return result;
     }
-}
+        }
