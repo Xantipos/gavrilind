242f958285f4e0e383785385553ef343f88d2532
Xantipos <anubite@bk.ru>
2018-05-20 17:15:52 +0300

    массив заполнен true или false по диагонали

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+/*
+* @author Dm Gavrilin (anubite@bk.ru)
+
+* @version $Id$
+ * @since 0.1
+*/
+public class Check {
+    public boolean mono(boolean[] data) {
+
+        boolean result = false;
+        int ind = 0;
+        for (int i=0; i<data.length; i++) {
+            if(data[0]=!data[i]){
+                ind++;
+            }
+        }
+        if (ind==0){
+            result=true;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+/*
+* @author Dm Gavrilin (anubite@bk.ru)
+
+* @version $Id$
+ * @since 0.1
+*/
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = false;
+        int ind = 0;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                if (data[0][0] = !data[i][j] && i == j) {
+                    ind++;
+                }
+            }
+
+        }
+            if (ind == 0) { result = true; }
+        return result;
+    }
+}
+
+
+
