ce9df0e43391f3bc0720affa753abac561e026f9
Xantipos <anubite@bk.ru>
2018-05-21 13:37:15 +0300

    2. Чтение кода и базовые ошибки. [#40205]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,34 +1,22 @@
 package ru.job4j.array;
 /*
 * @author Dm Gavrilin (anubite@bk.ru)
-
 * @version $Id$
  * @since 0.1
 */
-
-
 public class ArrayChar {
     private char[] data;
-
     public ArrayChar(String line) {
-        this.data = line.toCharArray();
-    }
-
-
+        this.data = line.toCharArray(); }
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-
-
         for (int index = 0; index <= value.length - 1; index++) {
             if (value[index] != data[index]) {
                 result = false;
                 break;
-
             }
-
         }
-
         return result;
     }
 }
--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -7,17 +7,12 @@ package ru.job4j.array;
 */
 public class Check {
     public boolean mono(boolean[] data) {
-
         boolean result = false;
         int ind = 0;
         for (int i=0; i<data.length; i++) {
-            if(data[0]=!data[i]){
-                ind++;
-            }
+            if(data[0]=!data[i]){ind++;}
         }
-        if (ind==0){
-            result=true;
-        }
+        if (ind==0){result=true;}
         return result;
     }
 }
