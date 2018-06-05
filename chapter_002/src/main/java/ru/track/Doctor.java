98b8502f1c748820748d239a90e754ca2bc8fd03
Xantipos <anubite@bk.ru>
2018-06-05 18:04:39 +0300

    Реализация профессий в коде #6837[#54946] еще раз

--- chapter_002/src/main/java/ru/track/Doctor.java
+++ chapter_002/src/main/java/ru/track/Doctor.java
@@ -4,11 +4,9 @@ public class Doctor extends Profession {
     public Doctor(String name, String profession) {
         super.name = "House M.D.";
         super.profession = "Doctor";
-        String disease = "h1n1";
     }
-    String diagnose = "healthy";
-    public String heal(String disease) {
 
-        return diagnose;
+    public String heal(Patient patient) {
+        return new Diagnose("Желтуха");
     }
 }
