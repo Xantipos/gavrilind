97e7fff29f52a19c55a58d0411b3d29bfcd779e2
Xantipos <mailto:anubite@bk.ru>
2018-08-16 16:57:28 +0300

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/tracker/input/ValidateInput.java
+++ chapter_002/src/main/java/ru/tracker/input/ValidateInput.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class ValidateInput implements Input {
 
     private final Input input;
+    public Validator validator = new Validator(new ConsoleInput());
 
     public ValidateInput(final Input input) {
         this.input = input;
@@ -16,6 +17,11 @@ public class ValidateInput implements Input {
     }
 
     public int ask(String question, int[] range){
+        if(validator.validate(question,range))
+            throw new NumberFormatException("Введите верное значение");
+            int value = this.input.ask(question, range);
+            return  value;
+        /**
         boolean invalid = true;
         int value = -1;
         do {
@@ -29,5 +35,6 @@ public class ValidateInput implements Input {
             }
         } while (invalid);
         return  value;
+         */
     }
 }
