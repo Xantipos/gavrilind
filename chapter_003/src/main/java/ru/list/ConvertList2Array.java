134d490b35fe0b57fd48def8d035f9f0a303cf6f
Xantipos <mailto:anubite@bk.ru>
2018-10-17 21:40:33 +0300

    3. Конвертация ArrayList в двухмерный массив2 [#10035]

--- chapter_003/src/main/java/ru/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/list/ConvertList2Array.java
@@ -9,16 +9,14 @@ public class ConvertList2Array {
         int ost = list.size() % rows > 0? 1 : 0;
         int cells = list.size()/rows + ost;
         int[][] array = new int[rows][cells];
-        List<Integer> clone = new ArrayList<Integer>(list.size());
-        for (Integer item : list) clone.add(item);
-
-        for (int k = 0; k != rows * cells - list.size(); k++) {clone.add(0);}
 
         int conter = 0;
         for (int j = 0; j != cells; j++) {
             for (int i = 0; i != rows; i++) {
                 conter = (i + j) + 2 * j;
-                array[j][i] = clone.get(conter);
+                if (conter < list.size()) {
+                    array[j][i] = list.get(conter);
+                } else break;
             }
         }
         return array;
