fc307f1e5ef14cf7e31b0a608482fe744d7a1beb
Xantipos <mailto:anubite@bk.ru>
2018-11-06 14:50:33 +0300

    3. Компаратор для строк. 2[#35008][#83269]

--- chapter_003/src/main/java/ru/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/comparator/ListCompare.java
@@ -6,15 +6,19 @@ public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        int koeff = Integer.compare(left.length(), right.length());
         int i = 0;
         while (i < left.length() & i < right.length()) {
             char chleft = left.charAt(i);
             char chright = right.charAt(i);
             int chcom = Character.compare(chleft, chright);
-            result = result + chcom;
+            if (chcom !=0) {
+                result = chcom;
+                break;
+            }
             i++;
         }
-        return result + koeff;
+        if (result == 0) {result = Integer.compare(right.length(), left.length());}
+
+        return result;
     }
 }
