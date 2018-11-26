4a765c04372d5d904ba4d8bf1b91a7da831ddf00
Xantipos <mailto:anubite@bk.ru>
2018-11-26 17:05:02 +0300

    Отсортировать департаменты [#34352]

---
+++ chapter_003/src/main/java/ru/sort/sortdepart.java
@@ -0,0 +1,41 @@
+package ru.sort;
+
+import java.util.Comparator;
+import java.util.*;
+
+public class Sortdepart implements Comparable<String> {
+    @Override
+    public int compareTo(String o) {
+        return this.compareTo(o);
+    }
+
+
+        public List<String> findDepartment(String[] in, int order) {
+            List result = new ArrayList<String>();
+            for (String st : in) {
+                if (!st.contains("/")) {
+                    result.add(st);
+                }
+            }
+            Collections.sort(result);
+            if (order == 1) { Collections.reverse(result);}
+
+            return result;
+        }
+
+        public List<TreeSet> addToDepart(String[] in, int order) {
+        List<String> departnames = findDepartment(in,order);
+        List<TreeSet> result = new ArrayList<>();
+        for(String stname : departnames){
+            TreeSet<String> dep = new TreeSet<>();
+            dep.add(stname);
+            for(String st : in){
+                if(st.contains(stname)){dep.add(st);}
+            }
+            result.add(dep);
+        }
+
+        return result;
+    }
+
+}
