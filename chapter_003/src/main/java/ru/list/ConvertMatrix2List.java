f2baf8aa9e8b796c3063d6b5a6d19ee33ba6145a
Xantipos <mailto:anubite@bk.ru>
2018-12-17 10:18:28 +0300

    1. Все задачи из 3 части переделать на Stream API. [#24259]

--- chapter_003/src/main/java/ru/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/list/ConvertMatrix2List.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
-        for(int[] i : array) {
-            list = Arrays.stream(i).boxed().collect(Collectors.toList());
-        }
+       // for(int[] i : array) {
+            //list = Arrays.stream(i).boxed().collect(Collectors.toList());
+            Arrays.stream(array).flatMap((p) -> Arrays.asList().stream()).collect(Collectors.toList());
+
+            Arrays.stream(array).flatMapToInt(x -> Arrays.stream(x));
+
+
+
+
+       // }
         return list;
     }
 }
